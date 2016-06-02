ef150fd7b20182fcf62ff577ae97bbec19904a6f
Sara Calhoun <scalhoun@baton2.compbio.ucsf.edu>
2016-06-01 17:46:43 -0700

    fixing merge conflicts

--- reaction_calculations.py
+++ reaction_calculations.py
@@ -315,8 +315,7 @@ class ligandData(object):
 
 
     def set_dock_scores_pd(self, scoresfile, reactionkey, idprefix='ZINC', id_column=1, score_column=7):
-        for i in self.molecules.keys():
-            ligdict[i] = 0.0
+        ligdict = {i:0.0 for i in self.molecules.keys()}
         with open(scoresfile, 'r') as handle:
             lines = handle.readlines()
             allscores = []
